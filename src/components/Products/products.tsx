51a1b1fd7d5d0c01018e98142347fec1bdd96c79
lvt10 <lucas.v.tome@gmail.com>
2023-09-20 22:45:16 +0100

    hotfix: tornando escalável a função

---
+++ src/components/Products/products.tsx
@@ -0,0 +1,44 @@
+import './products.css';
+
+import products from '../../utils/productsList.json';
+
+
+export const Products = () => {
+    return (
+        <section className="section" id="books">
+            <div className="container">
+                <h2 className="is-title green-text has-text-weight-bold has-text-centered is-size-1"> Nossas Publicações </h2>
+                <br />
+            </div>
+            <div className="container" id='product'>
+                <div className="masonry">
+                    {products.map((element: {_idPro: number; books:boolean; book_name:string; products_image:boolean; link1:string; name_products:string}) => {
+                        return(
+                            <div className="item">
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <div className="media">
+                                                {element.products_image ? ( 
+                                                <div className="media-left"> 
+                                                    <figure className="image is-128x128">
+                                                        <img src={"https://s3-sa-east-1.amazonaws.com/espacoresiliencia.com/profissionais/" + element._idPro + ".jpg"}  alt={element.name_products}></img>
+                                                    </figure>
+                                                </div>
+                                                ):<></> } 
+                                                <div className="media-content">
+                                                    <p className="title is-size-5-desktop is-size-4-tablet is-size-6-mobile">{element.name_products}</p>
+                                                    <p>{element.books ? (
+                                                        <a target='._blank' href={element.link1}  >{element.book_name}</a>
+                                                    ):<></>}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>           
+    )
+}
